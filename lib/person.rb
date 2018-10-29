af9d08b6591be69304c1dbd948b6873095526156
Certil Remy <certil@noukod.com>
2018-10-29 19:14:44 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
    attr_accessor :name, :age, :location, :user_name
 
     def initialize(attributes)
-
+    attributes.each {|key, value| self.send(("#{key}="), value)}
     end
   #your code here
 end
